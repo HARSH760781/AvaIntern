d273d036823406c89668a6fb451dfcf58dd19a98
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2023-04-26 17:35:41 +0530

    adding git ignore

--- app.js
+++ app.js
@@ -1,41 +1,86 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var hbs = require("hbs");
 
 var app = express();
 
+const templatePath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
+
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(path.join(__dirname, "./public/stylesheets")));
+app.use(express.static(path.join(__dirname, "./public/images")));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", templatePath);
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/career", (req, res) => {
+  res.render("career");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/career", (req, res) => {
+  res.render("career");
+});
+app.get("/course", (req, res) => {
+  res.render("course");
+});
+app.get("/course/ai", (req, res) => {
+  res.render("ai");
+});
+app.get("/course/ml", (req, res) => {
+  res.render("ml");
+});
+app.get("/course/app", (req, res) => {
+  res.render("app");
+});
+app.get("/course/cad", (req, res) => {
+  res.render("cad");
+});
+app.get("/course/cyber", (req, res) => {
+  res.render("cyber");
+});
+app.get("/course/ds", (req, res) => {
+  res.render("ds");
+});
+app.get("/course/language", (req, res) => {
+  res.render("language");
+});
+app.get("/course/marketing", (req, res) => {
+  res.render("marketing");
+});
+app.get("/course/mern", (req, res) => {
+  res.render("mern");
+});
+app.get("/course/python", (req, res) => {
+  res.render("python");
+});
+app.get("/course/robot", (req, res) => {
+  res.render("robot");
+});
+app.get("/course/web", (req, res) => {
+  res.render("web");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(80, () => {
+  console.log("Connection running");
 });
-
-module.exports = app;
